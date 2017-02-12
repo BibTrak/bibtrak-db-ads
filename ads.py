468975b9ce8d68cb4368daffffd27ce9c7340356
Sharlene Mendez <sharlenemendez@gmail.com>
2017-02-12 10:15:30 -0500

    Add files via upload

--- ads.py
+++ ads.py
@@ -2,40 +2,45 @@ import urllib.request
 import json
 import pprint
 
-#def fetch(id):
-auth_token = input("Enter Authorization Code: ")
-bibcode= input("Bibcode: ")
+import bibtrak import handler
 
-#def set_up(auth_, id_)
-url = (
-	"https://api.adsabs.harvard.edu/v1/search/query?q=" +
-	bibcode + 
-	"&fl=" +
-	",".join([
-		"author",
-		"title",
-		"id",
-		"keyword",
-		"pubdate",
-		"year",
-	])
-)
-req = urllib.request.Request(url)
-req.add_header("Authorization", "Bearer " + auth_token)
-response = urllib.request.urlopen(req)
-res = response.read()
-paper = json.loads(res.decode())
-bib_info = paper['response']['docs'][0]
-
-#def mod_dir()
-bib_info["bibcode"] = bibcode
-bib_info["adsurl"] = "http://adsabs.harvard.edu/abs/" + bibcode
-
-pprint.pprint(bib_info)
-
-
-
-
-
-
+class ADS(handler.Handler):
+	def fetch(self,id):
+		auth_token = self
+		bibcode = id
+		
+		#request access to the file 
+		url = (
+			"https://api.adsabs.harvard.edu/v1/search/query?q=" +
+			bibcode + 
+			"&fl=" +
+			",".join([
+				"author",
+				"title",
+				"id",
+				"keyword",
+				"pubdate",
+				"year",
+			])
+		)
+		req = urllib.request.Request(url)
+		req.add_header("Authorization", "Bearer " + auth_token)
+		
+		#open page and store info
+		response = urllib.request.urlopen(req)
+		res = response.read()
+		
+		#creating and setting the dictionary
+		paper = json.loads(res.decode())
+		bib_info = paper['response']['docs'][0]
+		
+		#modifying dict
+		bib_info["bibcode"] = bibcode
+		bib_info["adsurl"] = "http://adsabs.harvard.edu/abs/" + bibcode
+		
+		return bib_info
+		
+	#def authenticate(self, id):
+		
+		
  
